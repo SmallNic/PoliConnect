be37861203a9691bb9116a4536426e6a63afcd76
SmallNic <nas231@nyu.edu>
2015-05-16 18:44:09 -0400

    minor edits to new forms

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,12 +6,22 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    binding.pry
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save!
+      redirect_to(question_path(@question))
+    else
+      render :new
+    end
+
+    binding.pry
   end
 
   def edit
@@ -22,4 +32,11 @@ class QuestionsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def question_params
+    return params[:question].permit(:title, :body, :user_id)
+  end
+
+
 end
