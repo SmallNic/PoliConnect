c3b4b5da3e211a8937298579a9cee16274ad6721
Brad Foster <btf5048@gmail.com>
2015-05-16 17:07:49 -0400

    initial view for questions index/add methods to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,22 @@
-class QuestionController < ApplicationController
+class QuestionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
